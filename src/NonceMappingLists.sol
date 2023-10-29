c27684142e52da8a3bdc3116c7a8d82a8c363394
Cory Gabrielsen <corygabrielsen@gmail.com>
2023-10-28 20:03:38 -0700

    Consolidate  modifier logic

--- src/NonceMappingLists.sol
+++ src/NonceMappingLists.sol
@@ -1,58 +1,27 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.20;
 
+import {ABaseLists} from "./ABaseLists.sol";
 import {MappingLists} from "./MappingLists.sol";
 import {IListRegistry} from "./IListRegistry.sol";
 import {ListRecord} from "./ListRecord.sol";
+import {NonceListManager} from "./NonceListManager.sol";
 
 /**
  * @title NonceMappingLists
  * @notice Manages records for each EFP List NFT, providing functionalities for record
  * manipulation. Employs a soft deletion mechanism, flagging records as deleted without removing them from storage.
  */
-contract NonceMappingLists is MappingLists {
-
-    /// @notice A mapping from a nonce to its associated manager.
-    mapping(uint nonce => address) public managers;
+contract NonceMappingLists is MappingLists, NonceListManager {
 
     /**
      * @notice A modifier that ensures only the manager of a specific token can access the decorated function.
      * @param nonce The nonce of the list.
      * @dev Throws an error if the caller isn't the manager of the provided nonce.
      */
-    modifier onlyListManager(uint nonce) override {
-        require(managers[nonce] == msg.sender, "Not manager");
+    modifier onlyListManager(uint nonce) override(ABaseLists, NonceListManager) {
+        _onlyListManager(nonce);
         _;
-    }
-
-    /**
-     * @notice Allows a manager to claim authority over a token using an offchain signature.
-     * @param nonce The nonce to claim.
-     */
-    function claimListManager(uint nonce) external {
-        // first come first serve can claim nonce
-        require(managers[nonce] == address(0), "Nonce already claimed");
-        managers[nonce] = msg.sender;
-    }
-
-    /**
-     * @notice Allows a manager to set the manager of a token.
-     *
-     * @param nonce The nonce of the list.
-     * @param manager The manager to set.
-     */
-    function setListManager(uint nonce, address manager) external onlyListManager(nonce) {
-        managers[nonce] = manager;
-    }
-
-    /**
-     * @notice Fetches the list manager associated with a specific token.
-     *
-     * @param nonce The nonce of the list.
-     * @return The list manager.
-     */
-    function getListManager(uint nonce) external view returns (address) {
-        return managers[nonce];
     }
 
 }
--- test/Gas.t.sol
+++ test/Gas.t.sol
@@ -6,6 +6,7 @@ import {DeletableListEntry} from "../src/ArrayLists.sol";
 import {NonceArrayLists} from "../src/NonceArrayLists.sol";
 import {NonceLogLists} from "../src/NonceLogLists.sol";
 import {NonceMappingLists} from "../src/NonceMappingLists.sol";
+import {ListOperation} from "../src/ListOperation.sol";
 import {ListRecord} from "../src/ListRecord.sol";
 
 contract NonceLogListsTest is Test {
@@ -16,8 +17,11 @@ contract NonceLogListsTest is Test {
     uint8 constant VERSION = 1;
     uint8 constant LIST_RECORD_TYPE_RAW_ADDRESS = 1;
     address ADDRESS_1 = 0x0000000000000000000000000000000000AbC123;
-    address ADDRESS_2 = 0x0000000000000000000000000000000000DeF456;
-    address ADDRESS_3 = 0x0000000000000000000000000000000000789AbC;
+
+    address[100] public addresses;
+
+    ListOperation[] public operations10;
+    ListOperation[] public operations100;
 
     function setUp() public {
         nonceArrayLists = new NonceArrayLists();
@@ -28,17 +32,69 @@ contract NonceLogListsTest is Test {
 
         nonceMappingLists = new NonceMappingLists();
         nonceMappingLists.claimListManager(NONCE);
+
+        // Generate dummy addresses for testing
+        for(uint i = 0; i < 100; i++) {
+            addresses[i] = address(uint160(i));
+        }
+
+        ListOperation[] memory _operations10 = _generateOperations(10);
+        for (uint i = 0; i < _operations10.length; i++) {
+            operations10.push(_operations10[i]);
+        }
+        ListOperation[] memory _operations100 = _generateOperations(100);
+        for (uint i = 0; i < _operations100.length; i++) {
+            operations100.push(_operations100[i]);
+        }
     }
 
-    function test_gasNonceArrayListsAppendRecord() public {
+    function _generateOperations(uint count) internal view returns (ListOperation[] memory ops) {
+        require(count <= 100, "Cannot generate more than 100 operations");
+
+        ops = new ListOperation[](count);
+        for(uint i = 0; i < count; i++) {
+            address addr = address(uint160(i)); // Generate a dummy address based on the loop index
+            ops[i] = ListOperation({
+                operationType: nonceArrayLists.OPERATION_APPEND(),
+                data: abi.encode(ListRecord(VERSION, LIST_RECORD_TYPE_RAW_ADDRESS, abi.encodePacked(addr)))
+            });
+        }
+    }
+
+
+    function test_appendRecord_nonceArrayLists() public {
         nonceArrayLists.appendRecord(NONCE, VERSION, LIST_RECORD_TYPE_RAW_ADDRESS, abi.encodePacked(ADDRESS_1));
     }
 
-    function test_gasNonceLogListsAppendRecord() public {
+    function test_appendRecord_nonceLogLists() public {
         nonceLogLists.appendRecord(NONCE, VERSION, LIST_RECORD_TYPE_RAW_ADDRESS, abi.encodePacked(ADDRESS_1));
     }
 
-    function test_gasNonceMappingListsAppendRecord() public {
+    function test_appendRecord_nonceMappingLists() public {
         nonceMappingLists.appendRecord(NONCE, VERSION, LIST_RECORD_TYPE_RAW_ADDRESS, abi.encodePacked(ADDRESS_1));
+    }
+
+    function test_modifyAppend10_nonceArrayLists() public {
+        nonceArrayLists.modifyRecords(NONCE, operations10);
+    }
+
+    function test_modifyAppend10_nonceLogLists() public {
+        nonceLogLists.modifyRecords(NONCE, operations10);
+    }
+
+    function test_modifyAppend10_nonceMappingLists() public {
+        nonceMappingLists.modifyRecords(NONCE, operations10);
+    }
+
+    function test_modifyAppend100_nonceArrayLists() public {
+        nonceArrayLists.modifyRecords(NONCE, operations100);
+    }
+
+    function test_modifyAppend100_nonceLogLists() public {
+        nonceLogLists.modifyRecords(NONCE, operations100);
+    }
+
+    function test_modifyAppend100_nonceMappingLists() public {
+        nonceMappingLists.modifyRecords(NONCE, operations100);
     }
 }
