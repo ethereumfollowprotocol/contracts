5dfb4b23fdfc7855d922f9673d723f9c98bab6cb
Cory Gabrielsen <corygabrielsen@gmail.com>
2023-11-25 17:27:02 -0800

    Add EFPListMinter

---
+++ src/beta/EFPListMinter.sol
@@ -0,0 +1,76 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+import {IEFPAccountMetadata} from "./IEFPAccountMetadata.sol";
+import {IEFPListMetadata} from "./IEFPListMetadata.sol";
+import {IEFPListRegistry} from "./IEFPListRegistry.sol";
+
+interface IEFPListRegistry_ is IEFPListRegistry {
+    function totalSupply() external view returns (uint);
+}
+
+contract EFPListMinter {
+    IEFPListRegistry_ public registry;
+    IEFPAccountMetadata public accountMetadata;
+    IEFPListMetadata public listMetadata;
+    address public listsAddressL1;
+
+    constructor(address _registryAddress, address _accountMetadataAddress, address _listMetadataAddress, address _listsAddressL1) {
+        registry = IEFPListRegistry_(_registryAddress);
+        accountMetadata = IEFPAccountMetadata(_accountMetadataAddress);
+        listMetadata = IEFPListMetadata(_listMetadataAddress);
+        listsAddressL1 = _listsAddressL1;
+    }
+
+    function mintWithListLocationOnL1AndSetAsDefaultList(uint nonceL1) public payable {
+        uint tokenId = registry.totalSupply();
+        registry.mint{
+            value: msg.value
+        }();
+        _setDefaultListForAccount(msg.sender, tokenId);
+        _setListLocationL1(tokenId, listsAddressL1, nonceL1);
+    }
+
+    function mintToWithListLocationOnL1AndSetAsDefaultList(address to, uint nonceL1) public payable {
+        uint tokenId = registry.totalSupply();
+        registry.mintTo{
+            value: msg.value
+        }(to);
+        _setDefaultListForAccount(to, tokenId);
+        _setListLocationL1(tokenId, listsAddressL1, nonceL1);
+    }
+
+    function mintWithListLocationOnL2AndSetAsDefaultList(uint chainId, address addressL2, uint nonceL2) public payable {
+        uint tokenId = registry.totalSupply();
+        registry.mint{
+            value: msg.value
+        }();
+        _setDefaultListForAccount(msg.sender, tokenId);
+        _setListLocationL2(tokenId, chainId, addressL2, nonceL2);
+    }
+
+    function mintToWithListLocationOnL2AndSetAsDefaultList(address to, uint chainId, address addressL2, uint nonceL2) public payable {
+        uint tokenId = registry.totalSupply();
+        registry.mintTo{
+            value: msg.value
+        }(to);
+        _setDefaultListForAccount(to, tokenId);
+        _setListLocationL2(tokenId, chainId, addressL2, nonceL2);
+    }
+
+    function _setDefaultListForAccount(address to, uint tokenId) internal {
+        accountMetadata.setValueForAddress(to, "efp.list.default", abi.encodePacked(tokenId));
+    }
+
+    function _setListLocationL1(uint tokenId, address addr, uint nonce) internal {
+        bytes1 version = 0x01;
+        bytes1 listLocationType = 0x01;
+        listMetadata.setValue(tokenId, "efp.list.location", abi.encodePacked(version, listLocationType, addr, nonce));
+    }
+
+    function _setListLocationL2(uint tokenId, uint chainId, address addr, uint nonce) internal {
+        bytes1 version = 0x01;
+        bytes1 listLocationType = 0x02;
+        listMetadata.setValue(tokenId, "efp.list.location", abi.encodePacked(version, listLocationType, chainId, addr, nonce));
+    }
+}
---
+++ test/beta/EFPListMinter.t.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+import "forge-std/Test.sol";
+import {EFPAccountMetadata} from "../../src/beta/EFPAccountMetadata.sol";
+import {EFPListMetadata} from "../../src/beta/EFPListMetadata.sol";
+import {EFPListRegistry} from "../../src/beta/EFPListRegistry.sol";
+import {EFPLists} from "../../src/beta/EFPLists.sol";
+import {EFPListMinter} from "../../src/beta/EFPListMinter.sol";
+
+contract EFPListMinterTest is Test {
+    EFPAccountMetadata public accountMetadata;
+    EFPListMetadata public listMetadata;
+    EFPListRegistry public registry;
+    EFPLists public lists;
+    EFPListMinter public minter;
+
+    uint NONCE_L1 = 1234;
+    bytes1 LIST_LOCATION_VERSION = bytes1(0x01);
+    bytes1 LIST_LOCATION_TYPE_L1 = bytes1(0x01);
+    bytes1 LIST_LOCATION_TYPE_L2 = bytes1(0x02);
+
+    function setUp() public {
+        accountMetadata = new EFPAccountMetadata();
+        listMetadata = new EFPListMetadata();
+        registry = new EFPListRegistry();
+        lists = new EFPLists();
+        listMetadata.setEFPListRegistry(address(registry));
+        registry.setMintState(EFPListRegistry.MintState.PublicMint);
+        registry.mint();
+
+        minter = new EFPListMinter(address(registry), address(accountMetadata), address(listMetadata), address(lists));
+        accountMetadata.addProxy(address(minter));
+    }
+
+    function test_CanMintWithListLocationOnL1AndSetAsDefaultList() public {
+        uint tokenId = registry.totalSupply();
+        minter.mintWithListLocationOnL1AndSetAsDefaultList(NONCE_L1);
+
+        assertEq(accountMetadata.getValue(address(this), "efp.list.default"), abi.encodePacked(tokenId));
+        assertEq(listMetadata.getValue(uint(tokenId), "efp.list.location"), abi.encodePacked(LIST_LOCATION_VERSION, LIST_LOCATION_TYPE_L1, address(lists), NONCE_L1));
+    }
+
+    function test_CanMintWithListLocationOnL2AndSetAsDefaultList() public {
+        uint chainId = 2222;
+        address addressL2 = address(0x4444444);
+        uint nonceL2 = 3333;
+        uint tokenId = registry.totalSupply();
+        minter.mintWithListLocationOnL2AndSetAsDefaultList(chainId, addressL2, nonceL2);
+
+        assertEq(accountMetadata.getValue(address(this), "efp.list.default"), abi.encodePacked(tokenId));
+        assertEq(listMetadata.getValue(uint(tokenId), "efp.list.location"), abi.encodePacked(LIST_LOCATION_VERSION, LIST_LOCATION_TYPE_L2, chainId, addressL2, nonceL2));
+    }
+}
