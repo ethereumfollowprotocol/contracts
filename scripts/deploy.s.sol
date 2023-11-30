39449e6dc0c5a78db965ec5e837c7a6262115fe9
Cory Gabrielsen <corygabrielsen@gmail.com>
2023-11-29 22:57:11 -0800

    Fix deployer script

--- scripts/deploy.s.sol
+++ scripts/deploy.s.sol
@@ -22,7 +22,8 @@ contract DeployScript is Script {
     }
 
     function run() public {
-        vm.startBroadcast();
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
 
         console.log(GREEN, "Deployer           :", msg.sender, ENDC);
         console.log();
