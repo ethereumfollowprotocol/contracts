2f265cabf263e9297ff6f35f83addfa8655f5490
omar <omaraziz.dev@proton.me>
2024-01-07 04:22:40 -0800

    enable publich-batch minting when deploying

--- env.d.ts
+++ env.d.ts
@@ -1,10 +1,10 @@
-declare module NodeJS {
-  interface ProcessEnv {
-    readonly NODE_ENV: 'development' | 'production' | 'test'
-    readonly ANVIL_ACCOUNT_PRIVATE_KEY: string;
-    readonly PRIVATE_KEY: string;
-    readonly ETHERSCAN_API_KEY: string
-    readonly SEPOLIA_ADDRESS: string
-    readonly SEPOLIA_ACCOUNT_PRIVATE_KEY: string
-  }
+declare namespace NodeJS {
+	interface ProcessEnv {
+		readonly NODE_ENV: "development" | "production" | "test";
+		readonly ANVIL_ACCOUNT_PRIVATE_KEY: `0x${string}`;
+		readonly PRIVATE_KEY: string;
+		readonly ETHERSCAN_API_KEY: string;
+		readonly SEPOLIA_ADDRESS: `0x${string}`;
+		readonly SEPOLIA_ACCOUNT_PRIVATE_KEY: string;
+	}
 }
---
+++ scripts/update-mint-state.ts
@@ -0,0 +1,39 @@
+#!/usr/bin/env bun
+import { clients } from './clients.ts'
+import * as abi from '../generated/abi.ts'
+import { privateKeyToAccount } from 'viem/accounts'
+
+/**
+ * cli usage
+ * node scripts/enable-mint.ts --public-mint|--public-batch|--owner-only|--disabled
+ */
+
+const mintStateRecord = {
+  '--disabled': 0,
+  '--owner-only': 1,
+  '--public-mint': 2,
+  '--public-batch': 3
+}
+
+const [, , command] = process.argv as [string, string, keyof typeof mintStateRecord]
+
+if (!mintStateRecord[command]) throw new Error('Invalid command')
+
+console.info(`\n\nSetting mint state to ${command}: ${mintStateRecord[command]}\n\n`)
+
+main()
+  .then(console.log)
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
+
+async function main() {
+  return clients.localhostAnvil().writeContract({
+    abi: abi.efpListRegistryAbi,
+    functionName: 'setMintState',
+    address: '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512',
+    account: privateKeyToAccount(Bun.env.ANVIL_ACCOUNT_PRIVATE_KEY),
+    args: [mintStateRecord[command]]
+  })
+}
