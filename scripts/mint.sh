879d32540ae4cdad61c5f244a58b483b2051fea3
Cory Gabrielsen <corygabrielsen@gmail.com>
2024-01-09 20:30:31 -0800

    Quality of life improvements in mint script

--- scripts/mint.sh
+++ scripts/mint.sh
@@ -14,10 +14,15 @@ function mint() {
   MINT_INITIAL_TOTAL_SUPPLY=${1} forge script scripts/mint.s.sol --fork-url ${ETHEREUM_LOCAL_NODE_URL:-http://localhost:8545} --broadcast --private-key ${PRIVATE_KEY} ${FORGE_ARGS}
 }
 
+start=50
+end=${MINT_INITIAL_TOTAL_SUPPLY:-50}
+step=50
+
+# we will pass this to the forge script ourselves
+unset MINT_INITIAL_TOTAL_SUPPLY
+
 # change to 1000 for large mint test
-for i in {0..150..50}
+for ((i=start; i<=end; i+=step))
 do
-  if [ $i -ne 0 ]; then
-    mint $i
-  fi
+  mint $i
 done
