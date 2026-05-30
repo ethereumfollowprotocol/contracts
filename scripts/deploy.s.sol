5384d43985dbbb20eea7127993a0ee2a9a27855c
Nejc Drobnic <nejc@nejc.dev>
2026-05-30 16:55:08 +0200

    refactor: Update SPDX license identifier to MIT in all Solidity files

--- scripts/deploy.s.sol
+++ scripts/deploy.s.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.23;
 
 import {console} from 'lib/forge-std/src/console.sol';
--- src/EFPListRegistry.sol
+++ src/EFPListRegistry.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.23;
 
 import {ERC721A} from 'lib/ERC721A/contracts/ERC721A.sol';
