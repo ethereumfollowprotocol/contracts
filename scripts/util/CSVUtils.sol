7140d248929f5001913afcd4b9dbaaccd3e23446
Nejc Drobnic <nejc@nejc.dev>
2026-05-31 07:42:34 +0200

    chore(solidity): update SPDX license identifier to UNLICENSED

--- scripts/util/CSVUtils.sol
+++ scripts/util/CSVUtils.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.23;
 
 import {BytesUtils} from './BytesUtils.sol';
--- src/EFPListRegistry.sol
+++ src/EFPListRegistry.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.23;
 
 import {ERC721A} from 'lib/ERC721A/contracts/ERC721A.sol';
